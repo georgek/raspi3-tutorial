a5ddf04e3ff4bc6a885962bd386b06eb301f3bd6
George Kettleborough <kettleg@gmail.com>
2019-03-25 22:20:23 +0000

    uart1 stuff

--- 03_uart1/uart.c
+++ 03_uart1/uart.c
@@ -48,20 +48,20 @@ void uart_init()
 
     /* initialize UART */
     *AUX_ENABLE |=1;       // enable UART1, AUX mini uart
-    *AUX_MU_CNTL = 0;
+    *AUX_MU_CNTL = 0;      // disable Tx, Rx
     *AUX_MU_LCR = 3;       // 8 bits
     *AUX_MU_MCR = 0;
-    *AUX_MU_IER = 0;
-    *AUX_MU_IIR = 0xc6;    // disable interrupts
+    *AUX_MU_IER = 0;       // disable interrupts
+    *AUX_MU_IIR = 0xc6;    // clear Rx and Tx FIFOs
     *AUX_MU_BAUD = 270;    // 115200 baud
     /* map UART1 to GPIO pins */
     r=*GPFSEL1;
     r&=~((7<<12)|(7<<15)); // gpio14, gpio15
     r|=(2<<12)|(2<<15);    // alt5
     *GPFSEL1 = r;
-    *GPPUD = 0;            // enable pins 14 and 15
+    *GPPUD = 0;            // disable pull/up down...
     r=150; while(r--) { asm volatile("nop"); }
-    *GPPUDCLK0 = (1<<14)|(1<<15);
+    *GPPUDCLK0 = (1<<14)|(1<<15); // ...for pins 14 and 15
     r=150; while(r--) { asm volatile("nop"); }
     *GPPUDCLK0 = 0;        // flush GPIO setup
     *AUX_MU_CNTL = 3;      // enable Tx, Rx
@@ -94,10 +94,11 @@ char uart_getc() {
  * Display a string
  */
 void uart_puts(char *s) {
-    while(*s) {
-        /* convert newline to carrige return + newline */
-        if(*s=='\n')
-            uart_send('\r');
-        uart_send(*s++);
-    }
+     while(*s) {
+          /* convert newline to carrige return + newline */
+          if(*s=='\n') {
+               uart_send('\r');
+          }
+          uart_send(*s++);
+     }
 }
